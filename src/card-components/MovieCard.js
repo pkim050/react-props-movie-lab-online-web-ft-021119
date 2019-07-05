feb4361fecf9bca318b1868df3702a7ef585560d
Patrick Kim <pkim050@gmail.com>
2019-07-05 11:49:31 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -42,5 +42,8 @@ export default class MovieCard extends Component {
 
 // Don't forget your default props!
 MovieCard.defaultProps {
-
+  title: 'Unknown'
+  IMDBRating: null
+  genres:
+  poster: 'default'
 }
